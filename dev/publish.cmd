ecf288e06857e38bb76b060dd7c17839ce38ef63
Alexander Sklar <asklar@microsoft.com>
2020-07-02 22:26:17 -0700

    publish only from master

--- dev/publish.cmd
+++ dev/publish.cmd
@@ -1,5 +1,16 @@
 @echo off
+setlocal
+
+for /f %%i in ('git branch --show-current') do set branch=%%i
+
+if NOT "%branch%"=="master" (
+    echo You must run this script from master. Current branch is %branch%
+    exit /b 1
+)
+
 call npx bump
 git commit -m "bump package.json" -i package.json
 git push
 npm publish
+
+endlocal
