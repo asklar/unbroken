16a795782b73ce6cd7e137903b41ce685fc90771
Alexander Sklar <asklar@winse.microsoft.com>
2020-12-16 22:37:36 -0800

    linting

--- test/test.ts
+++ test/test.ts
@@ -1,4 +1,4 @@
-import * as unbroken from '../lib/checker'
+import * as unbroken from '../lib/checker';
 import chalk from 'chalk';
 import * as fs from 'fs';
 import * as path from 'path';
@@ -9,44 +9,44 @@ interface TestCase {
     options: unbroken.Options
 }
 
-function AssertAreEqual(a: any, b: any, testcase: string) {
-    if (a !== b) {
-        console.log(testcase, chalk.redBright(`Expected ${a}, actual ${b}`));
-        throw new Error(`${testcase} - expected ${a}, actual ${b}`);
-    }
-    return true;
+function AssertAreEqual (a: any, b: any, testcase: string) {
+  if (a !== b) {
+    console.log(testcase, chalk.redBright(`Expected ${a}, actual ${b}`));
+    throw new Error(`${testcase} - expected ${a}, actual ${b}`);
+  }
+  return true;
 }
 
 const TestCases = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'TestCases.json')).toString()).TestCases as TestCase[];
 
-async function Test(option?: string | undefined) {
-    console.log();
-    let nErrors = 0;
+async function Test (option?: string | undefined) {
+  console.log();
+  let nErrors = 0;
 
-    const testCases = (!option || option === '-l') ? TestCases : TestCases.filter(x => x.name === option);
+  const testCases = (!option || option === '-l') ? TestCases : TestCases.filter(x => x.name === option);
 
-    for (const testcase of testCases) {
-        const checker = new unbroken.Checker(testcase.options);
-        try {
-            if (!process.env.CI) {
-                process.stdout.write(testcase.name + ' ');
-                process.stdout.cursorTo(0);
-            }
-            if (option !== '-l' || testcase.options['local-only']) {
-                const v = await checker.Process();
-                if (AssertAreEqual(testcase.expected, v, testcase.name)) {
-                    console.log(testcase.name, chalk.greenBright('ok'));
-                }
-            } else if (option === '-l') {
-                // the test was not local but we are running only local tests
-                console.log(testcase.name, chalk.grey('skipped'));
-            }
-        } catch (e) {
-            console.error(e);
-            nErrors++;
+  for (const testcase of testCases) {
+    const checker = new unbroken.Checker(testcase.options);
+    try {
+      if (!process.env.CI) {
+        process.stdout.write(testcase.name + ' ');
+        process.stdout.cursorTo(0);
+      }
+      if (option !== '-l' || testcase.options['local-only']) {
+        const v = await checker.Process();
+        if (AssertAreEqual(testcase.expected, v, testcase.name)) {
+          console.log(testcase.name, chalk.greenBright('ok'));
         }
+      } else if (option === '-l') {
+        // the test was not local but we are running only local tests
+        console.log(testcase.name, chalk.grey('skipped'));
+      }
+    } catch (e) {
+      console.error(e);
+      nErrors++;
     }
-    process.exitCode = nErrors;
+  }
+  process.exitCode = nErrors;
 }
 
 Test(process.argv[2]);
