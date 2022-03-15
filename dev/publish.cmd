7d12e288be151082d6798b47fc76d0ebac877597
Jon Thysell (JAUNTY) <jthysell@microsoft.com>
2022-03-15 11:40:20 -0700

    Cleanup and new workflows for better unbroken
    * Removed npm commands and files, now using yarn
    * Removed old beachball dependency and change files
    * New CI and publish workflows based on tags
    * Updated publish script to invoke a new release

--- dev/publish.cmd
+++ dev/publish.cmd
@@ -1,17 +1,6 @@
 @echo off
 setlocal
 
-for /f %%i in ('git branch --show-current') do set branch=%%i
-
-if NOT "%branch%"=="master" (
-    echo You must run this script from master. Current branch is %branch%
-    exit /b 1
-)
-
-call npx bump
-git commit -m "bump package.json" -i package.json
-git push
-call yarn build
-npm publish
+call node %~dp0\publish.js
 
 endlocal
